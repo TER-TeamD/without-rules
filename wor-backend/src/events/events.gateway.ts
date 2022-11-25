a78389e196158552803fbf2d03c452921ea3619d
William D'Andrea <71140800+william-dandrea@users.noreply.github.com>
2022-11-25 11:29:26 +0100

    Adf one route

--- wor-backend/src/events/events.gateway.ts
+++ wor-backend/src/events/events.gateway.ts
@@ -5,10 +5,8 @@ import {
   WebSocketServer,
   WsResponse,
 } from '@nestjs/websockets';
-import { from, Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
 import { Server } from 'socket.io';
-import {OnModuleInit} from "@nestjs/common";
+import { OnModuleInit } from '@nestjs/common';
 
 @WebSocketGateway({
   cors: {
--- wor-backend/src/game-engine/controllers/game-engine.controller.ts
+++ wor-backend/src/game-engine/controllers/game-engine.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import {Controller, Post} from '@nestjs/common';
+import {GameEngineService} from "../services/game-engine.service";
 
 @Controller('game-engine')
-export class GameEngineController {}
+export class GameEngineController {
+
+
+    constructor(private readonly gameEngineService: GameEngineService) {}
+
+
+    @Post('/start-game')
+    public async startGame() {
+        return {
+            "status": "Game is launched"
+        }
+    }
+
+
+
+}
