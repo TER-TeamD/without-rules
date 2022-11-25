a7912e9de78b4c04d09fecbd9c33949a13318dc3
William D'Andrea <71140800+william-dandrea@users.noreply.github.com>
2022-11-25 10:06:19 +0100

    Implement the server

--- wor-backend/src/app.module.ts
+++ wor-backend/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GameEngineModule } from './game-engine/game-engine.module';
+import {EventsModule} from "./events/events.module";
 
 @Module({
-  imports: [GameEngineModule],
+  imports: [GameEngineModule, EventsModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ wor-backend/src/events/events.gateway.ts
@@ -0,0 +1,37 @@
+import {
+  MessageBody,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+  WsResponse,
+} from '@nestjs/websockets';
+import { from, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Server } from 'socket.io';
+import {OnModuleInit} from "@nestjs/common";
+
+@WebSocketGateway({
+  cors: {
+    origin: '*',
+  },
+})
+export class EventsGateway implements OnModuleInit{
+  @WebSocketServer()
+  server: Server;
+
+  onModuleInit(): any {
+    this.server.on('connection', socket => {
+      console.log(socket.id);
+      console.log("Connected")
+    })
+  }
+
+  @SubscribeMessage('newMessage')
+  onNewMessage(@MessageBody() body: any) {
+    console.log(body)
+    this.server.emit('newMessage', {
+      msg: "New message",
+      content: body
+    })
+  }
+}
