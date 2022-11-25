b0f070b32052625708a683be966666b82dc244fe
William D'Andrea <71140800+william-dandrea@users.noreply.github.com>
2022-11-25 11:02:14 +0100

    Add mongoose

--- wor-backend/src/app.module.ts
+++ wor-backend/src/app.module.ts
@@ -2,10 +2,25 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GameEngineModule } from './game-engine/game-engine.module';
-import {EventsModule} from "./events/events.module";
+import { EventsModule } from './events/events.module';
+import { MongooseConfigService } from './shared/services/mongoose-config.service';
+import mongodbConfig from './shared/config/mongodb.config';
+import appConfig from './shared/config/app.config';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [GameEngineModule, EventsModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [appConfig, mongodbConfig],
+    }),
+    MongooseModule.forRootAsync({
+      useClass: MongooseConfigService,
+    }),
+    GameEngineModule,
+    EventsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
