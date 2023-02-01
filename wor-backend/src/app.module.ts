bba41cdcb5ad3632b45e1764f0acfc76f095305a
YannClodong <yann.clodong@gmail.com>
2023-02-01 09:00:28 +0100

    :bug: Fix import bugs

--- wor-backend/src/app.module.ts
+++ wor-backend/src/app.module.ts
@@ -2,12 +2,8 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GameEngineModule } from './game-engine/game-engine.module';
-import { MongooseConfigService } from './shared/services/mongoose-config.service';
-import mongodbConfig from './shared/config/mongodb.config';
-import appConfig from './shared/config/app.config';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
-import swaggeruiConfig from "./shared/config/swaggerui.config";
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://database:27017'),
--- wor-backend/src/main.ts
+++ wor-backend/src/main.ts
@@ -1,18 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
-import {SwaggerUIConfig} from "./shared/config/inferfaces/swaggerui-config.interface";
-import {ValidationPipe} from "@nestjs/common";
-import {ConfigService} from "@nestjs/config";
 
 async function bootstrap() {
-
   const app = await NestFactory.create(AppModule);
   app.enableCors();
 
-
-  await app.listen(3000).then(c => {
-    console.log(3000)
+  await app.listen(3000).then(() => {
+    console.log(3000);
   });
 }
 bootstrap();
