32ea394b5cd498823590d6ed146155be99704d6e
YannClodong <yann.clodong@gmail.com>
2023-02-23 17:18:05 +0100

    🤡 Add missing change

--- frontend/wor-table-frontend/lib/services/network/models/chronometer_data.dart
+++ frontend/wor-table-frontend/lib/services/network/models/chronometer_data.dart
@@ -2,6 +2,7 @@ class ChronometerData {
   final DateTime startTime = DateTime.now();
   final DateTime endTime;
   Duration get interval => endTime.difference(startTime);
+  bool get expired => DateTime.now().isAfter(endTime);
 
   ChronometerData(this.endTime);
 }
