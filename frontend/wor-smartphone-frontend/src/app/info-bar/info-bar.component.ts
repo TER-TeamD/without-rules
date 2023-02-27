7b48fad62cb9e42e90367f240b1bdbb5ef8a8c58
William D'Andrea <71140800+william-dandrea@users.noreply.github.com>
2023-02-27 11:44:41 +0100

    Corrrection du bug de changement de couleur dsur le telephone

--- frontend/wor-smartphone-frontend/src/app/info-bar/info-bar.component.ts
+++ frontend/wor-smartphone-frontend/src/app/info-bar/info-bar.component.ts
@@ -19,20 +19,26 @@ export class InfoBarComponent {
   public urlAvatar: string = "";
   public cattleHeads: number = 0;
 
-  public colorCattleHeadBackground: string = '#FFF'
+
+  public colorCattleHeadBackground: string = '#ececec'
 
 
   ngOnInit(): void {
     this.urlAvatar = `/assets/avatars/${this.player?.avatar}.png`;
 
+    const currentCattlehead = this.cattleHeads;
     this.cattleHeads = 0;
+
     this.player?.in_player_game_property?.player_discard.forEach(card => {
       this.cattleHeads += card.cattleHead;
-      this.colorCattleHeadBackground = '#ff9c9c';
-      setTimeout(() => {
-        this.colorCattleHeadBackground = '#FFF';
-      }, 2000)
-
     });
+
+    if (currentCattlehead != this.cattleHeads) {
+      this.colorCattleHeadBackground = '#ff9c9c';
+
+      setTimeout(() => {
+        this.colorCattleHeadBackground = '#ececec';
+      }, 3000)
+    }
   }
 }
